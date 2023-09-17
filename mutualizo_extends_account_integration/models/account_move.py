4368de17142232891ab81d7c692420bc9ba53954
joaovictor1212 <joaovictor.1212@hotmail.comom>
2023-09-17 20:16:05 -0300

    Atualização do modulo account_integration

--- mutualizo_extends_account_integration/models/account_move.py
+++ mutualizo_extends_account_integration/models/account_move.py
@@ -1,7 +1,8 @@
 
 from odoo import fields, _, api, models
 from odoo.exceptions import ValidationError, UserError
-
+import requests
+import json
 
 
 
@@ -15,6 +16,20 @@ class AccountMove(models.Model):
 
         for record in self:
             if record:
-                pass
+                integration_data = {
+                    'external_id' : record.id,
+                }
+            
+             # Envie os dados para o sistema externo via API REST
+            response = requests.post('URL_DO_SISTEMA_EXTERNO_API', json=integration_data)
+            
+            if response:
+            # Atualize o registro no modelo account.invoice.integration
+                integration_record = self.env['account.invoice.integration'].create({
+                    'invoice_id': record.id,
+                    'external_id': response.id,
+                    'status': response.status_code,  # Status da resposta do sistema externo
+                    'response_message': response.text  # Mensagem de resposta do sistema externo
+                })
 
         return res
